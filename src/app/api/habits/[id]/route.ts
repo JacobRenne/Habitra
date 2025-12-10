9aedec166706dc46ce04fd6c7b66d5aed3cada50
Jacob Rennemark <jacobrane.se@gmail.com>
2025-12-10 20:39:15 +0100

    fix delete route

--- src/app/api/habits/[id]/route.ts
+++ src/app/api/habits/[id]/route.ts
@@ -4,13 +4,13 @@ import { getUserIdFromRequest } from "@/lib/getUserIdFromRequest";
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { id: string } },
+  { params }: { params: Promise<{ id: string }> },
 ) {
-  const { id } = params;
+  const { id } = await params;
+
   const userId = await getUserIdFromRequest(req);
   if (!userId)
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-
   // verify ownership
   const habit = await prisma.habit.findUnique({ where: { id } });
   if (!habit || habit.userId !== userId)
